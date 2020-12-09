1c533ed3efcc69c2df3d20171a4ac8672bcd6868
alexdevteam <alexpro820@gmail.com>
2020-12-09 09:33:17 +0100

    Puzzle 9

--- src/puzzle9.rs
+++ src/puzzle9.rs
@@ -4,11 +4,57 @@ use std::io::{Cursor, BufRead};
 use regex::Regex;
 #[allow(unused_imports)]
 use std::str::FromStr;
-use std::collections::HashSet;
-
-#[macro_use] mod instructions;
-use instructions::*;
+use std::collections::VecDeque;
 
 fn main() {
-    let mut instructions = read_instructions!("inputs/puzzle9.txt");
+    const BUFFER_SIZE: usize = 25;
+    let cursor = Cursor::new(include_str!("inputs/puzzle9.txt"));
+    let mut data = Vec::new();
+    for line in cursor.lines() {
+        let line = line.unwrap();
+        let int = usize::from_str(line.as_str()).unwrap();
+        data.push(int);
+    }
+    let check_if_sum = |index: usize| -> bool {
+        for i in (index-BUFFER_SIZE).max(0)..index {
+            for j in (index-BUFFER_SIZE).max(0)..index {
+                if i == j { continue; }
+                if data[index] == data[i] + data[j] { return true; }
+            }
+        }
+        false
+    };
+    let invalid_num: usize = {
+        let mut result = 0;
+        for i in BUFFER_SIZE..data.len() {
+            if !check_if_sum(i) {
+                println!("Invalid number: {}", data[i]);
+                result = data[i];
+                break;
+            }
+        }
+        result
+    };
+    for i in 0..data.len() {
+        for size in 1..data.len()-i {
+            let mut sum: usize = 0;
+            let mut smallest: usize = 9999999;
+            let mut largest: usize = 0;
+            for j in i..i+size {
+                sum += data[j];
+                if data[j] < smallest {
+                    smallest = data[j];
+                }
+                if data[j] > largest {
+                    largest = data[j];
+                }
+            }
+            if sum == invalid_num {
+                println!("{} found, {}..{}", sum, i, i+size-1);
+                println!("Smallest + largest: {}", smallest + largest);
+                return;
+            }
+        }
+    }
+    println!("Found no matches");
 }
